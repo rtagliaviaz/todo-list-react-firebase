72fd45af792c830b37bd327443b0a50b0c8ca2cd
Robert Tagliavia <robert5638@gmail.com>
2019-06-12 12:35:08 -0400

    Update config.js
    done

--- src/config.js
+++ src/config.js
@@ -5,13 +5,13 @@ import 'firebase/firebase-auth';
 
 // Initialize Firebase
 firebase.initializeApp({
-  apiKey: "AIzaSyD9bONxgWMHtCxP8TPlWtLf3IiPQe5pORI",
+  apiKey: "apikey",
   authDomain: "todo-app-38ae6.firebaseapp.com",
-  databaseURL: "https://todo-app-38ae6.firebaseio.com",
-  projectId: "todo-app-38ae6",
-  storageBucket: "todo-app-38ae6.appspot.com",
-  messagingSenderId: "421815607242",
-  appId: "1:421815607242:web:1651af78a40e7750"
+  databaseURL: "authDomain",
+  projectId: "projectId",
+  storageBucket: "storageBucket",
+  messagingSenderId: "messagingSenderId",
+  appId: " appId"
 });
 
 let db = firebase.firestore();
